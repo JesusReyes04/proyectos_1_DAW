af57fd01b1e4da5e07821f5fd366c7d47d562b5f
JesusReyes04 <jesusreyesespejo04@gmail.com>
2024-04-18 16:57:02 +0200

    Juego casi casi terminado
    Está casi terminado

--- EneEnRaya2/JuegoNRaya.java
+++ EneEnRaya2/JuegoNRaya.java
@@ -19,6 +19,7 @@ public class JuegoNRaya{
         selecionarPrimerJugador();
         tablero = new Tablero();
         tablero.crearTablero();
+        tablero.mostrarTablero();
     }
     
     public void jugar(){
@@ -45,9 +46,9 @@ public class JuegoNRaya{
         String nombreDelJugador1;
         do{
             System.out.println("¿Quién va a ser el jugador 1?");
-            System.out.print(jugadores[0].getNombre()+" o "+jugadores[1] + " ");
+            System.out.print(jugadores[0].getNombre()+" o "+jugadores[1].getNombre() + " ");
             nombreDelJugador1 = leer.nextLine();
-        }while(nombreDelJugador1!=jugadores[0].getNombre()&&nombreDelJugador1!=jugadores[1].getNombre());
+        }while(!nombreDelJugador1.equals(jugadores[0].getNombre())&&!nombreDelJugador1.equals(jugadores[1].getNombre()));
         if (nombreDelJugador1.equals(jugadores[0].getNombre())) {
             jugadores[0].setTurno(true);
         } else {
@@ -58,14 +59,11 @@ public class JuegoNRaya{
 
     public void crearJugadores(){
         Scanner leer = new Scanner(System.in);
-        String nombre;
-        char ficha;
         for (int i = 0; i < jugadores.length; i++) {
             System.out.println("Introduce tu nombre: ");
-            nombre = leer.nextLine();
+            String nombre = leer.nextLine();
             System.out.println("Introduce la ficha con la que vas a jugar");
-            ficha = leer.next().charAt(0);
-            // arreglar aquí que se queda pillao con lo del Scanner
+            char ficha = leer.nextLine().charAt(0);
             jugadores[i] = new Jugador(nombre);
             jugadores[i].setFicha(ficha);
             System.out.println("Ok " + jugadores[i].getNombre()+" vas a jugar con "+jugadores[i].getFicha());
